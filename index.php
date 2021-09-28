ad1a9890893f5e0ccc48c2af627a3505b1c88763
Stefani Castellanos <scct95@gmail.com>
2021-09-28 08:01:46 -0500

    Improve error handling and hide error reporting on prod

--- index.php
+++ index.php
@@ -4,6 +4,17 @@ require __DIR__.'/vendor/autoload.php';
 
 use App\Exceptions\TwilioExceptionHandler;
 
-new TwilioExceptionHandler();
+// Load environment variables from .env, or environment if available
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$DISPLAY_ERRORS = $_ENV['DISPLAY_ERRORS'];
+ini_set('display_errors', $DISPLAY_ERRORS);
+
+$accountSid =  $_ENV['TWILIO_ACCOUNT_SID'];
+$authToken =  $_ENV['TWILIO_AUTH_TOKEN'];
+$twilioNumber =  $_ENV['TWILIO_NUMBER'];
+
+new TwilioExceptionHandler($accountSid, $authToken, $twilioNumber);
 
 throw new Exception('Uh oh server error');
